abd5bc8558ff3eb9b33b7eb76b38d65ba8e369ea
yashwanthrao425@gmail.com <yashwanthrao425@gmail.com>
2024-04-24 21:15:36 +0530

    methodTwo methodThree added in publicclass1

--- src/accessmodifier1/publicclass1.java
+++ src/accessmodifier1/publicclass1.java
@@ -12,5 +12,14 @@ abstract class publicclass1 {
 	public static void methodOne() {
 		System.out.println("This is public class code");
 	}
+	
+	public static void methodTwo() {
+		System.out.println("This is public class code");
+	}
+	
+	public static void methodThree() {
+		System.out.println("This is public class code");
+	}
+	
 
 }
