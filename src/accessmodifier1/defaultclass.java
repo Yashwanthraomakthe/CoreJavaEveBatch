f74dfac92991d716a8ba829a0ab534f09426173b
yashwanthrao425@gmail.com <yashwanthrao425@gmail.com>
2024-04-24 21:22:35 +0530

    test

--- src/accessmodifier1/defaultclass.java
+++ src/accessmodifier1/defaultclass.java
@@ -1,20 +1,20 @@
 package accessmodifier1;
 
- class defaultclass {
+class defaultclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	public static void methodtwo() {
 		System.out.println("This is default class code");
 	}
-	
+
 	public static void methodOne() {
 		System.out.println("This is default class code");
 	}
-	
+
 	public static void methodThree() {
 		System.out.println("This is default class code");
 	}
